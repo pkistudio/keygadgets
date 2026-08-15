2f3c88af341d01ae34596fabb5868183fda1888e
dweller long gone <dwellerlonggone@outlook.com>
2026-08-15 00:44:19 +0000

    Prepare release 0.1.1

--- src/version.ts
+++ src/version.ts
@@ -2,4 +2,4 @@ declare const __KEYGADGETS_VERSION__: string | undefined;
 
 export const KEY_GADGETS_VERSION = typeof __KEYGADGETS_VERSION__ === 'string'
   ? __KEYGADGETS_VERSION__
-  : '0.1.0';
+  : '0.1.1';
